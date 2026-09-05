114d75930c70a7779d780a9da53dee3d7708ae90
mark <markkinuthia53@gmail.com>
2026-09-05 14:45:00 +0300

    feat: update user access messages for clarity based on age verification

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
+// store the name and age of the user
 let name = "";
 let age =0;
 
+//Display the name and age of the user in the console
 console.log(name);
 console.log(age);
 
+// calculate the age in months
 let months = calculateAgeInMonths(age);
 
+// display the age in months
 console.log(months);
 
+// function to convert age from years to months
 function calculateAgeInMonths(age) {
     return age * 12;
 }
 
+// check if the user is an adult or a minor
 if (age >=18){
-    console.log("You can access all available sokoni features");
+    console.log("You can access adult content on Sokoni");
 } else{
-    console.log("Some Sokoni features may require adult supervision");
+    console.log("You are too young for adult content on Sokoni");
 }
 
+//loop to display a motivational message 5 times
 for(let i = 0; i < 5; i++){
     console.log("Keep going! You're almost there!");
 }
